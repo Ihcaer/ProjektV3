43ddfbed18798c18f3b884be5cc69080db0f9ac5
Ihcaer <jakub2002410@wp.pl>
2023-11-27 23:43:08 +0100

    Zmiana animacji menu artykuly

--- src/app/views/cms/cms.component.ts
+++ src/app/views/cms/cms.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 import { Router, RouterOutlet } from '@angular/router';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { trigger, state, style, animate, transition, query, stagger } from '@angular/animations';
 
 import { CmsPodswietlenieSMService } from '../../services/cms-podswietlenie-sm/cms-podswietlenie-sm.service';
 import { MenuKontaComponent } from '../../components/menu-konta/menu-konta.component';
@@ -14,7 +14,7 @@ import { MenuKontaComponent } from '../../components/menu-konta/menu-konta.compo
   styleUrl: './cms.component.scss',
   imports: [CommonModule, MenuKontaComponent, RouterOutlet],
   animations: [
-    trigger('otwieranieMenu', [
+    /*trigger('otwieranieMenu', [
       transition(':enter', [
         style({ opacity: 1, height: 0 }),
         animate('0.3s ease', style({ opacity: 1, height: '*' }))
@@ -23,19 +23,29 @@ import { MenuKontaComponent } from '../../components/menu-konta/menu-konta.compo
         style({ opacity: 1, height: '*' }),
         animate('0.3s ease', style({ opacity: 1, height: 0 }))
       ])
+    ]),*/
+    trigger('otwieranieMenu', [
+      transition('* => *', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateY(-15px)' }),
+          stagger('100ms', [
+            animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+          ]),
+        ], { optional: true }),
+      ]),
     ]),
     trigger('obrotStrzalki', [
       state('closed', style({
         height: '*',
         opacity: 1,
         overflow: 'visible',
-        transform: 'rotate(0deg)' // Początkowa rotacja
+        transform: 'rotate(0deg)'
       })),
       state('open', style({
         height: '*',
         opacity: 1,
         overflow: 'visible',
-        transform: 'rotate(180deg)' // Końcowa rotacja
+        transform: 'rotate(180deg)'
       })),
       transition('closed <=> open', [
         animate('0.3s ease')
@@ -44,7 +54,6 @@ import { MenuKontaComponent } from '../../components/menu-konta/menu-konta.compo
   ],
 })
 export class CmsComponent implements OnInit {
-
 
   constructor(private titleService: Title, private router: Router, protected podswietlenieSM: CmsPodswietlenieSMService, private cdRef: ChangeDetectorRef) {
     this.titleService.setTitle('CMS');
