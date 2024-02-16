ac5b1abb0a7fb9d4180af0742f052633f142153d
Ihcaer <jakub2002410@wp.pl>
2024-02-16 19:01:53 +0100

    Dodanie komunikatów o bledach logowania

---
+++ frontend/src/app/components/blad-logowania-snackbar/blad-logowania-snackbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject, Input } from '@angular/core';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+
+interface Message {
+  index: number;
+  text: string;
+}
+
+@Component({
+  selector: 'app-blad-logowania-snackbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './blad-logowania-snackbar.component.html',
+  styleUrl: './blad-logowania-snackbar.component.scss'
+})
+export class BladLogowaniaSnackbarComponent {
+  message: Message;
+
+  messages: Message[] = [
+    { index: 0, text: 'Nieprawidłowe dane logowania.' },
+    { index: 1, text: 'Pola logowania nie mogą być puste.' }
+  ];
+
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any) {
+    const messageIndex = data?.messageIndex;
+    this.message = this.messages.find(msg => msg.index === messageIndex) || { index: -1, text: 'Brak wiadomości' };
+  }
+}
