bee0d26b87ceffd056064e45fc927e4bba6ef93b
Ihcaer <jakub2002410@wp.pl>
2024-02-28 02:33:10 +0100

    Dodanie ikon permisji w tabeli pracownikow

--- frontend/src/app/views/pracownicy/pracownicy.component.ts
+++ frontend/src/app/views/pracownicy/pracownicy.component.ts
@@ -20,21 +20,14 @@ import { UsuwaniePracownikaDialogComponent } from '../../components/usuwanie-pra
 export class PracownicyComponent implements OnInit {
   employees: any[] = [];
 
-  /*permissionsIcons: { [key: number]: string[] } = {
-    1: ['news'],
-    2: ['rule'],
-    4: ['price_change'],
-    15: ['user_attributes'],
-    30: ['co_present'],
-    60: ['settings_account_box'],
-  };*/
+  permissionsIcons: string[] = ['feed', 'rule', 'price_change', 'people', 'co_present', 'engineering'];
 
-  /*uprawnienia1 = [1, 3, 5, 7, 16, 18, 20, 31, 35, 46, 61, 65, 66, 76, 91, 112];
+  uprawnienia1 = [1, 3, 5, 7, 16, 18, 20, 31, 35, 46, 61, 65, 66, 76, 91, 112];
   uprawnienia2 = [2, 3, 6, 7, 17, 18, 21, 32, 33, 36, 47, 62, 63, 66, 77, 92, 112];
   uprawnienia3 = [5, 6, 7, 19, 20, 21, 34, 35, 36, 49, 64, 65, 66, 79, 94, 112];
   uprawnienia4 = [16, 17, 18, 19, 20, 21, 45, 46, 47, 49, 75, 76, 77, 79, 105, 112];
   uprawnienia5 = [31, 32, 33, 34, 35, 36, 45, 46, 47, 49, 90, 91, 92, 94, 105, 112];
-  uprawnienia6 = [61, 62, 63, 64, 65, 66, 75, 76, 77, 79, 90, 91, 92, 94, 105, 112];*/
+  uprawnienia6 = [61, 62, 63, 64, 65, 66, 75, 76, 77, 79, 90, 91, 92, 94, 105, 112];
 
   constructor(private employeesTableService: EmployeesTableService, private router: Router, private cdr: ChangeDetectorRef, public dialog: MatDialog, private cmsAuthService: AuthCMSService) { }
 
@@ -66,5 +59,47 @@ export class PracownicyComponent implements OnInit {
         return;
       }
     })
+  }
+
+  getPermissionIcon(permission: number): string[] {
+    let icons: string[] = [];
+
+    if (this.uprawnienia1.includes(permission)) {
+      icons.push(this.permissionsIcons[0]);
+    }
+    if (this.uprawnienia2.includes(permission)) {
+      icons.push(this.permissionsIcons[1]);
+    }
+    if (this.uprawnienia3.includes(permission)) {
+      icons.push(this.permissionsIcons[2]);
+    }
+    if (this.uprawnienia4.includes(permission)) {
+      icons.push(this.permissionsIcons[3]);
+    }
+    if (this.uprawnienia5.includes(permission)) {
+      icons.push(this.permissionsIcons[4]);
+    }
+    if (this.uprawnienia6.includes(permission)) {
+      icons.push(this.permissionsIcons[5]);
+    }
+    return icons
+  }
+  getPermissionIconTooltip(icon: string) {
+    switch (icon) {
+      case this.permissionsIcons[0]:
+        return 'Pisanie artykułów/edycja/usuwanie swoich artykułów';
+      case this.permissionsIcons[1]:
+        return 'Przegląd/wysyłanie/edycja/usuwanie wszystkich artykułów';
+      case this.permissionsIcons[2]:
+        return 'Dodawanie/edycja/usuwanie ofert franczyz';
+      case this.permissionsIcons[3]:
+        return 'Dodawanie/edycja/usuwanie użytkowników';
+      case this.permissionsIcons[4]:
+        return 'Dodawanie/edycja/usuwanie przedstawicieli';
+      case this.permissionsIcons[5]:
+        return 'Dodawanie/edycja/usuwanie pracowników';
+      default:
+        return '';
+    }
   }
 }
